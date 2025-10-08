44cefe5ad4936a618999cbaf756d0c944a956e5e
dannyrational <drodas6232@gmail.com>
2025-10-07 22:38:06 -0600

    Added Tailwind CSS to update styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
 import PortfolioState from "@/src/Context";
 import PreLoader from "@/src/layout/PreLoader";
 import "@/styles/globals.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 
 export default function App({ Component, pageProps }) {
   return (
